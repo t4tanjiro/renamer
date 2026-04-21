df03be2eccd01c4cb08cc8799351753cb85e71c9
t4tanjiro <t15361174@gmail.com>
2026-04-21 11:18:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6457709809:AAFJIMkpTiusK1ZMacL9H0CsuMAysNVGWf8")
+API_ID = int(os.environ.get("API_ID", "14050586"))
+API_HASH = os.environ.get("API_HASH", "42a60d9c657b106370c79bb0a8ac560c")
+OWNER = int(os.environ.get("OWNER", "7958077163"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "renamer_4gb_fast_ibot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "tbotz_update")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002317259468"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://rahul:rahulkr@cluster0.szdpcp6.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQDWZRoAD0OVtbKE9Z0826_u4jT1uNwYUPtTGRw26DQYCdHKLPDW6xLsioyl26ICZShzTiBA9dv6ZR0Te2gfDaqH5nFpHh7NOQSUMu7EJjB8X0v_yIV_CKJGcr6z6SJKPJb7sUmveNvXmYrMgG5jA7MuoYaqaDf0rZ61JNHvsUeRsYF2M7XEZlN-trAVtEExdUdXPIzczkev2LQ0UrA2JVN1znLbVLUhgnoHYOvsoe8NJ-fknI7KE5WSeBWyh70WQpHTlPuG0P9wXwh0jVWAaiI9sAd9q5rD2kmTX39AtF2m5XzMmYcYhRP0VguPVm6eMCmQP67wGp0e7SBktTQmNE6bMTAMSQAAAAHaVp7rAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
